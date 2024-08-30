ac5d6add64491738547536e0909af7023155c87a
johnicassere <johnicassere@hotmail.com>
2024-08-30 12:57:15 -0300

    figma

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 const api = 'https://fake-api-tau.vercel.app/api/efood/restaurantes'
 
-
+const figma = 'https://www.figma.com/design/JjduV2Tg713TzYUUsees8b/efood?node-id=1-7&node-type=FRAME&t=KoWZHlFCpA7xxRhh-0'
 
 export default api
