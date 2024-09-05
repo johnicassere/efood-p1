42f6a3a925514463fc040e335663d4b59f41d54f
johnicassere <johnicassere@hotmail.com>
2024-09-05 15:58:11 -0300

    init p3

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,33 @@
-const api = 'https://fake-api-tau.vercel.app/api/efood/restaurantes'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+
+
+const api = createApi({
+    baseQuery: fetchBaseQuery({
+        baseUrl: `https://fake-api-tau.vercel.app/api/efood/restaurantes`
+    }),
+
+    endpoints: (builder) => ({
+        getFeaturedRestaurantes: builder.query({
+           query: () => 'cart' 
+        }) 
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+//const api = 'https://fake-api-tau.vercel.app/api/efood/restaurantes'
 
 const figma = 'https://www.figma.com/design/JjduV2Tg713TzYUUsees8b/efood?node-id=1-7&node-type=FRAME&t=KoWZHlFCpA7xxRhh-0'
 
-export default api
+//export default api
 
 
